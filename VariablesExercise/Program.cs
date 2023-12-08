6af1543eeccadfce1bb9b4d1f03d5fcfaff77a6a
Hhertilien1 <kinghertilien@gmail.com>
2023-12-08 12:01:22 -0500

    Declared and initialized a few vairables, then added a few sentences.

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string brothersName = "Cliff";
+            int brothersAge = 23;
+            char brothersInitial = 'C';
+            bool brotherIsMale = true;
+            double brothersWeight = 154.25;
+            decimal brothersPrice = 54.99m;
+            Console.WriteLine($"Hi! I have brother named {brothersName}, who is {brothersAge} years old.");
+            Console.WriteLine($"It is {brotherIsMale} that his sex is male. He also weighs {brothersWeight}lbs.");
+            Console.WriteLine($"{brothersName}'s name starts with a {brothersInitial}. {brothersName} also sells custom desks starting at ${brothersPrice}.");
         }
     }
 }
